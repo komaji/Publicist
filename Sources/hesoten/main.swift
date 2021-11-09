dcb915498c7b0565a4271858f75a891ecff1de59
komaji <don.kojikoj@gmail.com>
2021-11-09 20:17:02 +0900

    Read file

--- Sources/hesoten/main.swift
+++ Sources/hesoten/main.swift
@@ -1,6 +1,12 @@
 import ArgumentParser
+import Foundation
 
 struct Command: ParsableCommand {
+    enum RunError: Error {
+        case invalidPath
+        case invalidFile
+    }
+
     static var configuration = CommandConfiguration(
       commandName: "hesoten",
       abstract: "hesoten!!!"
@@ -11,6 +17,19 @@ struct Command: ParsableCommand {
 
     func run() throws {
         print(filePath)
+
+        guard let fileHandle = FileHandle(forReadingAtPath: filePath) else {
+            throw RunError.invalidPath
+        }
+
+        let sourceData = fileHandle.readDataToEndOfFile()
+        defer { fileHandle.closeFile() }
+
+        guard let text = String(data: sourceData, encoding: .utf8) else {
+            throw RunError.invalidFile
+        }
+
+        print(text)
     }
 }
 
