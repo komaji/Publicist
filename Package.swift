9cf26abc512cac38f7314db007b9eacf823889d0
komaji <don.kojikoj@gmail.com>
2021-11-09 19:04:41 +0900

    Use ArgumentParser

--- Package.swift
+++ Package.swift
@@ -6,15 +6,15 @@ import PackageDescription
 let package = Package(
     name: "hesoten",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "hesoten",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
         .testTarget(
             name: "hesotenTests",
             dependencies: ["hesoten"]),
--- Sources/hesoten/main.swift
+++ Sources/hesoten/main.swift
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct Command: ParsableCommand {
+    static var configuration = CommandConfiguration(
+      commandName: "hesoten",
+      abstract: "hesoten!!!"
+    )
+
+    func run() throws {
+        print("Hello, world!")
+    }
+}
+
+Command.main()
