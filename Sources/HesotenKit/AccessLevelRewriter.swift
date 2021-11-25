9b43fe00429c2b2709009f182c0bd92f5083dd56
komaji <don.kojikoj@gmail.com>
2021-11-25 20:12:36 +0900

    Address enum

--- Sources/HesotenKit/AccessLevelRewriter.swift
+++ Sources/HesotenKit/AccessLevelRewriter.swift
@@ -75,4 +75,39 @@ public class AccessLevelRewriter: SyntaxRewriter {
             return super.visit(newNode)
         }
     }
+
+    override public func visit(_ node: EnumDeclSyntax) -> DeclSyntax {
+        let modifiers = node.modifiers ?? SyntaxFactory.makeModifierList([])
+
+        if modifiers.hasPrivateOrFileprivateModifier {
+            return DeclSyntax(node)
+
+        } else {
+            let newNode: EnumDeclSyntax
+
+            if modifiers.hasPublicModifier {
+                newNode = node
+
+            } else {
+                let newModifiers: ModifierListSyntax?
+
+                if let internalModifier = modifiers.firstInternalModifier {
+                    newModifiers = modifiers.replacing(
+                        childAt: internalModifier.indexInParent,
+                        with: SyntaxFactory.makePublicKeywordModifier()
+                    )
+
+                } else {
+                    newModifiers = modifiers.appending(SyntaxFactory.makePublicKeywordModifier())
+                }
+
+                newNode = node
+                    .withLeadingTrivia(.zero) // remove leading newlines
+                    .withModifiers(newModifiers) // add public keyword
+                    .withLeadingTrivia(node.leadingTrivia ?? .zero) // add leading newlines
+            }
+
+            return super.visit(newNode)
+        }
+    }
 }
